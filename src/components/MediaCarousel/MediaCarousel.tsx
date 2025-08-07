f43e4a5f0b567ffdb266162eb530c9edb8231a93
Austin Stewart <astewart@texasdevs.com>
2025-08-06 23:30:41 -0500

    Side gradients

--- src/components/MediaCarousel/MediaCarousel.tsx
+++ src/components/MediaCarousel/MediaCarousel.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./MediaCarousel.scss";
 import MediaCard, { type MediaCardProps } from "../MediaCard/MediaCard";
+import classNames from "classnames";
 
 type MediaCarouselProps = {
   title?: string;
@@ -14,16 +15,46 @@ const MediaCarousel: React.FC<MediaCarouselProps> = ({ title, items }) => {
     scrollRef.current?.scrollBy({ left: offset, behavior: "smooth" });
   };
 
+  const [canScrollLeft, setCanScrollLeft] = useState(false);
+  const [canScrollRight, setCanScrollRight] = useState(false);
+
+  useEffect(() => {
+    const el = scrollRef.current;
+    if (!el) return;
+
+    const checkScroll = () => {
+      setCanScrollLeft(el.scrollLeft > 0);
+      setCanScrollRight(el.scrollLeft + el.clientWidth < el.scrollWidth);
+    };
+
+    checkScroll();
+    el.addEventListener("scroll", checkScroll);
+    window.addEventListener("resize", checkScroll);
+
+    return () => {
+      el.removeEventListener("scroll", checkScroll);
+      window.removeEventListener("resize", checkScroll);
+    };
+  }, []);
+
+  const carouselClasses = classNames("media-carousel-container", {
+    "hide-left": !canScrollLeft,
+    "hide-right": !canScrollRight,
+  });
+
   return (
     <div className="media-carousel">
       {title && <h3 className="media-carousel-title">{title}</h3>}
-      <div className="media-carousel-container">
-        <button
-          className="media-carousel-arrow left"
-          onClick={() => scrollBy(-300)}
-        >
-          &lt;
-        </button>
+      <div className={carouselClasses}>
+        {canScrollLeft && (
+          <button
+            className="media-carousel-arrow left"
+            onClick={() => scrollBy(-300)}
+          >
+            &lt;
+          </button>
+        )}
+
         <div className="media-carousel-track" ref={scrollRef}>
           {items.map((item, i) => (
             <div key={i} className="media-carousel-item">
@@ -31,12 +62,14 @@ const MediaCarousel: React.FC<MediaCarouselProps> = ({ title, items }) => {
             </div>
           ))}
         </div>
-        <button
-          className="media-carousel-arrow right"
-          onClick={() => scrollBy(300)}
-        >
-          &gt;
-        </button>
+        {canScrollRight && (
+          <button
+            className="media-carousel-arrow right"
+            onClick={() => scrollBy(300)}
+          >
+            &gt;
+          </button>
+        )}
       </div>
     </div>
   );
